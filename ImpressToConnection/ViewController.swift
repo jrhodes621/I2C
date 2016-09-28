ff84d4aa02e251638eb39244006caed2bd188e78
James Rhodes <james@somehero.com>
2016-09-28 10:22:42 -0400

    Initial Commit

---
+++ ImpressToConnection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImpressToConnection
+//
+//  Created by James Rhodes on 9/28/16.
+//  Copyright © 2016 James Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
